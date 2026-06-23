ce64c852b2c1133d4d876ec751cfd9a0c438c475
SalticHash <saltichash@gmail.com>
2026-06-23 11:31:20 -0600

    Add Wheel Controls
    Added basic wheel controls, separated sprite 3d logic to its own script

--- player/player.gd
+++ player/player.gd
@@ -4,56 +4,45 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+const MAX_SPEED = 32
+var speed: float = 0.0
+var last_dir: float = 1.0
+func animate(delta: float) -> void:
+	$HamsterWheel.rotate_x(speed * PI / 32 * delta)
+	$Sprite.walking = speed != 0
+	var rot = create_tween()
+	rot.tween_property($Sprite, "rotation_degrees", Vector3(0, 0 if last_dir == 1 else 180, 0), 0.2)
+	$Sprite.walk_speed = remap(abs(speed), 0, MAX_SPEED, 0.2, 0.025)
 
-func get_angle(angle: float):
-	var a = rad_to_deg(angle_difference(0.0, deg_to_rad(angle)) + PI)
-	#$Sprite.rotation_degrees.y = 
-	$Sprite.rotation_degrees = Vector3(0, snappedf(angle, 45.0), 0)
-	match snappedf(a, 45.0):
-		0.0: return "north"
-		45.0: return "northwest"
-		90.0: return "west"
-		135.0: return "southwest"
-		180.0: return "south"
-		225.0: return "southeast"
-		270.0: return "east"
-		315.0: return "northeast"
-		360.0: return "north"
-	return "wow"
-
-var frame = 0
-var buffer = 0.0
 func _physics_process(delta: float) -> void:
-	if buffer < 0:
-		buffer = 0.1
-		frame += 1
-		frame %= 4
-	buffer -= delta
-	
-	var new_animation = get_angle(_camera_pivot.rotation_degrees.y)
-	if $Sprite.animation != new_animation:
-		buffer = 0.1
-	$Sprite.animation = new_animation
-	$Sprite.frame = frame
+
 
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 #
 	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var accel_dir: float = Input.get_axis("ui_down", "ui_up")
+	var turn_dir: float = Input.get_axis("ui_left", "ui_right")
+	if turn_dir: rotate_y(-turn_dir * PI * delta) 
+
+	_camera_pivot.global_position = global_position
+	if accel_dir:
+		last_dir = sign(accel_dir)
+		speed = move_toward(speed, MAX_SPEED * accel_dir, delta * 8)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		speed = move_toward(speed, 0.0, delta * 8)
+	
+	animate(delta)
+		
+	var direction := (transform.basis * Vector3.BACK).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
 
 	move_and_slide()
 
---
+++ player/sprite.gd
@@ -0,0 +1,49 @@
+extends AnimatedSprite3D
+
+
+## In radians.
+func get_cam_angle() -> float:
+	var cam = get_viewport().get_camera_3d()
+	var cam_pos = Vector2(cam.global_position.x, cam.global_position.z)
+	var pos = Vector2(global_position.x, global_position.z)
+	var cam_angle = cam_pos.angle_to_point(pos)
+	return cam_angle
+
+## Gets the cardinal name of the angle for the animations.
+func get_angle_name(angle: float):
+	var a = angle_difference(0.0, -angle) + PI
+	match snappedf(rad_to_deg(a), 45.0):
+		0.0: return "north"
+		45.0: return "northwest"
+		90.0: return "west"
+		135.0: return "southwest"
+		180.0: return "south"
+		225.0: return "southeast"
+		270.0: return "east"
+		315.0: return "northeast"
+		360.0: return "north"
+	return "error"
+
+
+var current_frame: int = 0 :
+	set(value):
+		current_frame = value % 4
+		frame = current_frame
+var walk_speed: float = 0.1
+var walk_frame_buffer: float = 0.0
+var walking: bool = false :
+	set(value):
+		walking = value
+		if walking == false: current_frame = 0
+
+func _process(delta: float) -> void:
+	var cam_angle: float = get_cam_angle()
+	var new_animation = get_angle_name(cam_angle + PI/2 + global_rotation.y)
+	if animation != new_animation:
+		walk_frame_buffer = walk_speed
+		animation = new_animation
+	
+	if walk_frame_buffer < 0.0:
+		walk_frame_buffer = walk_speed
+		current_frame += 1
+	if walking: walk_frame_buffer -= delta
